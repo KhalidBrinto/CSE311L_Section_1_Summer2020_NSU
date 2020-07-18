951f6b9366db1e78077a40a140b0270ac3ad56e8
KhalidBrinto <khalid.shafiq@northsouth.edu>
2020-07-18 10:00:25 +0600

    Lab assessment 2 Done

--- Lab Asssessment Submission/Lab Assessment 2/Connection.php
+++ Lab Asssessment Submission/Lab Assessment 2/Connection.php
@@ -1,3 +1,11 @@
 <?php
+$MySQLi_connection = new MySQLi('localhost','root','','test');
+if($MySQLi_connection->connect_error)
+{
+    echo "Not Connected, error" .$MySQLi_connection->connect_error;
+}
 
-$MySQLi_Connection = new MySQLi('localhost','root','test')
+else{
+    echo "Connected";
+}
+?>
